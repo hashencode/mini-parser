659648af32408ad8d83977510a2a31e70989377d
hashencode <hashencode@gmail.com>
2021-11-13 16:16:52 +0800

    修改打包格式

--- rollup.build.config.js
+++ rollup.build.config.js
@@ -2,20 +2,22 @@ import babel from "@rollup/plugin-babel";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 import del from "rollup-plugin-delete";
+import commonjs from '@rollup/plugin-commonjs';
 
 const config = {
   input: "src/main.ts",
   output: {
     file: "dist/index.js",
-    format: "es",
+    format: "cjs",
     sourcemap: false,
   },
   plugins: [
     del({ targets: "dist/*" }),
     typescript(),
+    commonjs(),
     babel({
       exclude: "node_modules/**",
-      babelHelpers: "bundled",
+      babelHelpers: "runtime",
     }),
     terser(),
   ],
