3722cd23b8fa14c8c51b80f7762ee61df14cbe20
guoshuaifeng <hashencode@gmail.com>
2022-01-21 12:28:58 +0800

    feat: 增加定时器，解决延时显示的问题

--- component/mini-parser/index.js
+++ component/mini-parser/index.js
@@ -10,29 +10,43 @@ Component({
     containerWidth: 0,
     renderVisible: false,
   },
+  queryTimer: null,
+  queryCount: 0,
   ready() {
     const { html, config } = this.data;
     const { adaptive = true } = config;
     if (html) {
       if (adaptive) {
-        // 获取外层容器宽度
-        wx.createSelectorQuery()
-          .in(this)
-          .select(".mini-parser")
-          .boundingClientRect((res) => {
-            if (res && res.width > 0) {
-              this.setData({
-                containerWidth: res.width,
-                renderVisible: true,
-              });
-            }
-          })
-          .exec();
+        this.queryTimer = setInterval(() => {
+          this.getContainerWidth();
+        }, 100);
       } else {
         this.setData({
           renderVisible: true,
         });
       }
     }
+  },
+  methods: {
+    getContainerWidth() {
+      if (this.queryTimer || this.queryCount > 20) {
+        // 获取外层容器宽度
+        wx.createSelectorQuery()
+          .in(this)
+          .select(".mini-parser")
+          .boundingClientRect((res) => {
+            if (res && res.width > 0) {
+              this.queryTimer = null;
+              this.setData({
+                containerWidth: res.width,
+                renderVisible: true,
+              });
+            } else {
+              this.queryCount += 1;
+            }
+          })
+          .exec();
+      }
+    },
   },
 });
