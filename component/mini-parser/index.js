a30506858bd0edd2a54d2689dcf792791ec2a242
guoshuaifeng <hashencode@gmail.com>
2022-03-01 11:50:38 +0800

    fix: 修复弹窗类型的富文本解析

--- component/mini-parser/index.js
+++ component/mini-parser/index.js
@@ -4,7 +4,7 @@ Component({
   },
   properties: {
     html: { type: String },
-    config: { type: Object, value: { adaptive: true } },
+    config: { type: Object, value: {} },
   },
   data: {
     containerWidth: 0,
@@ -13,39 +13,59 @@ Component({
   queryTimer: null,
   queryCount: 0,
   ready() {
-    const { html, config } = this.data;
-    const { adaptive = true } = config;
-    if (html) {
-      if (adaptive) {
-        this.queryTimer = setInterval(() => {
-          this.getContainerWidth();
-        }, 100);
-      } else {
-        this.setData({
-          renderVisible: true,
-        });
-      }
-    }
+    this.init();
+  },
+  observers: {
+    // 适应异步请求富文本数据
+    html() {
+      this.init();
+    },
   },
   methods: {
+    init() {
+      const { html, config } = this.data;
+      const { adaptive = true } = config;
+      if (html) {
+        if (adaptive) {
+          this.queryCount = 0;
+          this.queryTimer = setInterval(() => {
+            this.getContainerWidth();
+          }, 100);
+        } else {
+          this.setData({
+            renderVisible: true,
+          });
+        }
+      }
+    },
     getContainerWidth() {
-      if (this.queryTimer || this.queryCount > 20) {
-        // 获取外层容器宽度
-        wx.createSelectorQuery()
-          .in(this)
-          .select(".mini-parser")
-          .boundingClientRect((res) => {
-            if (res && res.width > 0) {
-              this.queryTimer = null;
-              this.setData({
-                containerWidth: res.width,
-                renderVisible: true,
-              });
-            } else {
-              this.queryCount += 1;
-            }
-          })
-          .exec();
+      if (this.queryTimer) {
+        if (this.queryCount < 10) {
+          // 获取外层容器宽度
+          wx.createSelectorQuery()
+              .in(this)
+              .select('.mini-parser')
+              .boundingClientRect(res => {
+                if (res && res.width > 0) {
+                  clearInterval(this.queryTimer);
+                  this.queryTimer = null;
+                  this.setData({
+                    containerWidth: res.width,
+                    renderVisible: true,
+                  });
+                } else {
+                  this.queryCount += 1;
+                }
+              })
+              .exec();
+        } else {
+          // 找不到外层元素时，将自适应设置为false
+          clearInterval(this.queryTimer);
+          this.setData({
+            config: { ...this.data.config, adaptive: false },
+            renderVisible: true,
+          });
+        }
       }
     },
   },
